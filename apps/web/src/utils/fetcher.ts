4d8ac84455d3087365df56bed5eeead137d4fc6a
stassig <stassig164@gmail.com>
2023-05-26 23:55:57 +0200

    header

--- apps/web/src/utils/fetcher.ts
+++ apps/web/src/utils/fetcher.ts
@@ -3,6 +3,7 @@ export const fetcher = async (url: string): Promise<any> => {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
+      "ngrok-skip-browser-warning": "true",
       Authorization:
         "Bearer " + (await fetch("/api/auth/token").then((res) => res.json())),
     },
