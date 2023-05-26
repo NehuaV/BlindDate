3b9dfd746d6b5fd7baad9350aae7ba6a45d7cfa3
stassig <stassig164@gmail.com>
2023-05-26 23:52:12 +0200

    header update

--- apps/web/src/utils/fetcher.ts
+++ apps/web/src/utils/fetcher.ts
@@ -2,6 +2,7 @@ export const fetcher = async (url: string): Promise<any> => {
   const response = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
       Authorization:
         "Bearer " + (await fetch("/api/auth/token").then((res) => res.json())),
     },
